dfbc947bb26729e50067cc102dbf967a39fd1963
Hiroshi Kimura <muukii.app@gmail.com>
2022-12-16 03:57:17 +0900

    Fix content mode (#2)

--- Sources/AsyncMultiplexImageDemo/Demo.swift
+++ Sources/AsyncMultiplexImageDemo/Demo.swift
@@ -1,13 +1,10 @@
 import AsyncMultiplexImage
 import AsyncMultiplexImage_Nuke
-import SwiftUI
 import Nuke
+import SwiftUI
 
+func buildURLs(baseURLString: String, size: CGSize) -> [URL] {
 
-func buildURLs() -> [URL] {
-  
-let baseURLString = "https://images.unsplash.com/photo-1492446845049-9c50cc313f00?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8"
-  
   var components = URLComponents(string: baseURLString)!
 
   return [
@@ -16,33 +13,94 @@ let baseURLString = "https://images.unsplash.com/photo-1492446845049-9c50cc313f0
     "w=50",
     "w=10",
   ].map {
-    
+
     components.query = $0
-    
+
     return components.url!
-    
+
   }
-  
+
 }
 
 struct AsyncMultiplexImage_Previews: PreviewProvider {
   static var previews: some View {
-    AsyncMultiplexImage(
-      urls: buildURLs(),
-      downloader: SlowDownloader(pipeline: .shared)
-    ) { phase in
-      switch phase {
-      case .empty:
-        Text("Loading")
-      case .progress(let image):
-        image
-      case .success(let image):
-        image
-          .resizable()
-          .scaledToFit()                  
-      case .failure(let error):
-        Text("Error")
+
+    Group {
+      AsyncMultiplexImage(
+        urlsProvider: { size in
+          buildURLs(
+            baseURLString:
+              "https://images.unsplash.com/photo-1492446845049-9c50cc313f00?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8",
+            size: size
+          )
+        },
+        downloader: SlowDownloader(pipeline: .shared)
+      ) { phase in
+        switch phase {
+        case .empty:
+          Rectangle()
+            .foregroundColor(.yellow)
+            .overlay(Text("Loading"))
+        case .progress(let image):
+          image
+            .resizable()
+            .scaledToFill()
+            .overlay(Text("Progress"))
+
+        case .success(let image):
+          image
+            .resizable()
+            .scaledToFill()
+            .overlay(Text("Done"))
+        case .failure(let error):
+          Text("Error")
+        }
       }
+      .frame(width: 300, height: 300)
+      .overlay(Color.red.opacity(0.3))
+    }
+  }
+}
+
+struct BookAlign: View, PreviewProvider {
+  var body: some View {
+    if #available(iOS 15, *) {
+      Content()
+    }
+  }
+
+  static var previews: some View {
+    Self()
+  }
+
+  @available(iOS 15, *)
+  private struct Content: View {
+
+    var body: some View {
+      ZStack {
+        AsyncImage(
+          url: .init(
+            string:
+              "https://images.unsplash.com/photo-1492446845049-9c50cc313f00?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8"
+          )!
+        ) { phase in
+          switch phase {
+          case .empty:
+            Rectangle()
+              .foregroundColor(.yellow)
+              .overlay(Text("Loading"))
+          case .success(let image):
+            image
+              .resizable()
+              .scaledToFill()
+              .overlay(Text("Done"))
+          case .failure(let error):
+            Text("Error")
+          }
+        }
+      }
+      .frame(width: 200, height: 200)
+      .overlay(Color.red.opacity(0.3))
     }
   }
 }
