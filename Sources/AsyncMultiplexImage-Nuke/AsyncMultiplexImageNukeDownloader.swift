bbb739e02294593a32c1ac146b1b5753a7ba6017
Muukii <muukii.app@gmail.com>
2023-01-17 18:06:24 +0900

    Add debug delay

--- Sources/AsyncMultiplexImage-Nuke/AsyncMultiplexImageNukeDownloader.swift
+++ Sources/AsyncMultiplexImage-Nuke/AsyncMultiplexImageNukeDownloader.swift
@@ -7,12 +7,23 @@ import SwiftUI
 public struct AsyncMultiplexImageNukeDownloader: AsyncMultiplexImageDownloader {
     
   public let pipeline: ImagePipeline
+  public let debugDelay: TimeInterval
   
-  public init(pipeline: ImagePipeline) {
+  public init(
+    pipeline: ImagePipeline,
+    debugDelay: TimeInterval
+  ) {
     self.pipeline = pipeline
+    self.debugDelay = debugDelay
   }
 
   public func download(candidate: AsyncMultiplexImageCandidate) async throws -> Image {
+    
+    #if DEBUG
+    
+    try? await Task.sleep(nanoseconds: UInt64(debugDelay * 1_000_000_000))
+    
+    #endif
     let response = try await pipeline.image(for: .init(urlRequest: candidate.urlRequest))
     return .init(uiImage: response.image)
   }
